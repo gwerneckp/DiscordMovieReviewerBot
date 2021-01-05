4a8f8e1b4b34bd43ec76db91082b53173ea08057
Zcode <gwerneckpaiva@gmail.com>
2021-01-05 19:42:13 +0000

    First working version.

---
+++ reaction.py
@@ -0,0 +1,51 @@
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+CLIENT_TOKEN = 'Nzk1NzM2MzI4NjMyMjcwODk5.X_NtNA.GAfYiBJ6vt_eq41bAqyE1a3i1pA'
+
+@client.event
+async def on_ready():
+    print('Hello_World')
+
+reactions = {"0️⃣": 0, "1️⃣": 1, "2️⃣": 2, "3️⃣": 3, "4️⃣": 4, "5️⃣": 5}
+
+@client.event
+async def on_message(message):
+    # if message.channel.id == 792940265873997885:
+     for attachment in message.attachments:
+      if attachment.filename.endswith('.jpg') or attachment.filename.endswith('.png') or attachment.filename.endswith('.jpeg'):
+        for reaction in reactions.keys():
+            await message.add_reaction(reaction)
+
+async def fetch_message(channel_id, message_id):
+    channel = client.get_channel(channel_id)
+    message = await channel.fetch_message(message_id)
+    return message
+
+async def reaction_add_remove(payload):
+        message = fetch_message(payload.channel_id, payload.message_id)
+        contador = 0
+        soma = 0
+        for reac in message.reactions:
+            i = reactions.get(reac.emoji, -1)
+            # se o emoji nao eh da nossa lista, vai para o proximo emoji
+            if i == -1:
+                continue
+            print("Emoji: ", i)
+            contador = reac.count - 1 + contador
+            soma = (reac.count-1)*i+soma
+        if contador > 0:
+            print("Media: ", soma/contador)
+        print("Numero de votos: ", contador)
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    await reaction_add_remove(payload)
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    await reaction_add_remove(payload)
+
+client.run(CLIENT_TOKEN)
