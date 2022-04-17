e993c2916dd7a2e0384cdac8e83ab573da07cd6a
gwern <gwerneckpaiva@gmail.com>
2022-04-17 02:55:11 +0100

    Added config file and fixed bugs.

---
+++ config.py
@@ -0,0 +1,24 @@
+#config options
+BOT_USER_ID = 0 #e: 123456789012345678
+ALLOWED_CHANNELS = [] #e: [000000000000000000, 000000000000000001, 000000000000000002]
+CLIENT_TOKEN = "" #e: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._12345"
+
+#ignore
+missing_info = False
+
+if BOT_USER_ID == 0:
+    print("You have not provided your bot's user id! Please provide one in config.py file.")
+    missing_info = True
+
+if ALLOWED_CHANNELS == [""]:
+    print("You have not selected what channels your bot will be operating on! Please provide at least one in config.py file.")
+    missing_info = True
+
+if CLIENT_TOKEN == "":
+    print("You have not provided your bot's client token! Please provide one in config.py file.")
+    missing_info = True
+
+if missing_info == True:
+    print("\n")
+    a = input('Press enter to exit ...\n')
+    exit(0)
