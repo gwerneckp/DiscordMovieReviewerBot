d1e2b06d99dffcd1cdc2a9408c944b52adb3c93e
gwerneckp <gwerneckpaiva@gmail.com>
2022-04-17 14:44:56 +0100

    Updated requirements.txt and config.py files

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 #config options
+
+# !IMPORTANT NO QUOTATIONS
 BOT_USER_ID = 0 #e: 123456789012345678
-ALLOWED_CHANNELS = [""] #e: [000000000000000000, 000000000000000001, 000000000000000002]
+# !IMPORTANT NO QUOTATIONS
+ALLOWED_CHANNELS = [] #e: [000000000000000000, 000000000000000001, 000000000000000002] 
 CLIENT_TOKEN = "" #e: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._12345"
 
 #ignore
@@ -10,7 +13,7 @@ if BOT_USER_ID == 0:
     print("You have not provided your bot's user id! Please provide one in config.py file.")
     missing_info = True
 
-if ALLOWED_CHANNELS == [""]:
+if ALLOWED_CHANNELS == []:
     print("You have not selected what channels your bot will be operating on! Please provide at least one in config.py file.")
     missing_info = True
 
